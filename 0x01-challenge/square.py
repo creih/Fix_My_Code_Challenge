68483b490f00f131f00270ba2e1db1f54ca4ed18
eRic Ishim <122928197+creih@users.noreply.github.com>
2024-05-15 23:41:45 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -21,7 +21,7 @@ class Square():
         """ Area of the square """
         return self.width * self.width
 
-    def permiter_of_mySquare(self):
+    def permiter_of_my_square(self):
         """ this is where the perimeter is calculated"""
         return (self.width + self.height) * 2
 
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_mySquare())
+    print(s.permiter_of_my_square())
